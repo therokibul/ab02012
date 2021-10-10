e14e8804531a5cee7539416af6807c02d8898229
therokibul <therokibul@gmail.com>
2021-10-10 16:29:42 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ void main() {
                 color: Colors.black54,
                 padding: EdgeInsets.all(10),
                 child: Text(
-                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic',
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. \n Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic',
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 20, color: Colors.white),
                 ),
