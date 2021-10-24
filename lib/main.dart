d2823365947cd58a9b756861023b1949118e41a9
therokibul <therokibul@gmail.com>
2021-10-24 17:02:13 +0600

    24-10-2021

--- lib/main.dart
+++ lib/main.dart
@@ -10,130 +10,100 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: ListView(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Center(
+            child: Column(
               children: [
-                Text(
-                  'Settings',
-                  style: TextStyle(color: Colors.white),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Container(
+                      height: 250,
+                      color: Colors.green,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Settings',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          'Profile',
+                          style: TextStyle(fontSize: 30, color: Colors.white),
+                        ),
+                        Text(
+                          'Settings',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Positioned(
+                      top: 150,
+                      left: 100,
+                      child: CircleAvatar(
+                        radius: 100,
+                        backgroundImage: AssetImage('images/undraw.png'),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 150,
                 ),
                 Text(
-                  'Profile',
+                  'Victoria Robertson',
+                  textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 30, color: Colors.white),
                 ),
                 Text(
-                  'Settings',
-                  style: TextStyle(color: Colors.white),
+                  'A mantra goes here',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  width: 350,
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Text(
+                    'Post',
+                    // textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                ListTile(
+                  leading: Icon(Icons.flutter_dash_outlined,
+                      size: 90, color: Colors.white),
+                  title: Text(
+                    'Header',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  subtitle: Text(
+                    "He'll want to use your yacht, and I don't want this thing smelling like fish.",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ],
             ),
-            SizedBox(
-              height: 25,
-            ),
-            CircleAvatar(
-              radius: 90,
-              backgroundImage: AssetImage('images/undraw.png'),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Text(
-              'Victoria Robertson',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 30, color: Colors.black),
-            ),
-            Text(
-              'A mantra goes here',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16, color: Colors.black),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            ListTile(
-              leading: Icon(Icons.flutter_dash_outlined,
-                  size: 90, color: Colors.white),
-              title: Text('Header'),
-              subtitle: Text(
-                  "He'll want to use your yacht, and I don't want this thing smelling like fish."),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            ListTile(
-              leading: Icon(Icons.flutter_dash_outlined,
-                  size: 90, color: Colors.white),
-              title: Text('Header'),
-              subtitle: Text(
-                  "He'll want to use your yacht, and I don't want this thing smelling like fish."),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            ListTile(
-              leading: Icon(Icons.flutter_dash_outlined,
-                  size: 90, color: Colors.white),
-              title: Text('Header'),
-              subtitle: Text(
-                  "He'll want to use your yacht, and I don't want this thing smelling like fish."),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            ListTile(
-              leading: Icon(Icons.flutter_dash_outlined,
-                  size: 90, color: Colors.white),
-              title: Text('Header'),
-              subtitle: Text(
-                  "He'll want to use your yacht, and I don't want this thing smelling like fish."),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            ListTile(
-              leading: Icon(Icons.flutter_dash_outlined,
-                  size: 90, color: Colors.white),
-              title: Text('Header'),
-              subtitle: Text(
-                  "He'll want to use your yacht, and I don't want this thing smelling like fish."),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            ListTile(
-              leading: Icon(Icons.flutter_dash_outlined,
-                  size: 90, color: Colors.white),
-              title: Text('Header'),
-              subtitle: Text(
-                  "He'll want to use your yacht, and I don't want this thing smelling like fish."),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            ListTile(
-              leading: Icon(Icons.flutter_dash_outlined,
-                  size: 90, color: Colors.white),
-              title: Text('Header'),
-              subtitle: Text(
-                  "He'll want to use your yacht, and I don't want this thing smelling like fish."),
-            ),
-
-            SizedBox(
-              height: 25,
-            ),
-            
-            ListTile(
-              leading: Icon(Icons.flutter_dash_outlined,
-                  size: 90, color: Colors.white),
-              title: Text('Header'),
-              subtitle: Text(
-                  "He'll want to use your yacht, and I don't want this thing smelling like fish."),
-            ),
-          ],
+          ),
         ),
       ),
     );
