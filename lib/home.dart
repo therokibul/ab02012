bfa3b4fa1cbfb859ead70474558af97df7b728c9
therokibul <therokibul@gmail.com>
2021-11-09 16:47:03 +0600

    11-09

--- lib/home.dart
+++ lib/home.dart
@@ -1,23 +1,44 @@
 import 'main.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+bool _changeButton = false;
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        color: Colors.teal,
-        child: TextButton(
-          onPressed: () {
-            Navigator.pushNamed(context, MyRoute.secondRoute);
-          },
-          child: Text(
-            'Go To Second Page',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 25,
+      child: Scaffold(
+        body: Center(
+          child: InkWell(
+            onTap: () {
+              setState(() {
+                _changeButton = !_changeButton;
+              });
+            },
+            child: AnimatedContainer(
+              duration: Duration(seconds: 5),
+              width: _changeButton ? 500 : 300,
+              height: _changeButton ? 500 : 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.green,
+              ),
+              child: _changeButton
+                  ? Icon(
+                      Icons.flutter_dash_outlined,
+                      size: 50,
+                    )
+                  : Text(
+                      'data',
+                      textScaleFactor: 3,
+                    ),
             ),
           ),
         ),
