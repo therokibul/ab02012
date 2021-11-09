af6263d79e6e62fd02e3b32e3b0a754eb54b05b5
therokibul <therokibul@gmail.com>
2021-11-09 16:57:58 +0600

    09-11

--- lib/home.dart
+++ lib/home.dart
@@ -37,6 +37,7 @@ class _HomePageState extends State<HomePage> {
                     )
                   : Text(
                       'data',
+                      textAlign: TextAlign.center,
                       textScaleFactor: 3,
                     ),
             ),
