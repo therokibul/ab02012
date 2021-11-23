95941824842bfa39d0187beaccf9940ce4358174
therokibul <therokibul@gmail.com>
2021-11-23 16:48:35 +0600

    23-11

---
+++ lib/home_desktop.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+class HomeDesktop extends StatelessWidget {
+  const HomeDesktop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    var screenHeight = screenSize.height;
+    var screenWidth = screenSize.width;
+    return Center(
+      child: Container(
+        height: screenHeight * 0.6,
+        width: screenWidth * 0.7,
+        color: Colors.green,
+        constraints: BoxConstraints(
+          maxWidth: 700,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
-import 'package:ab02012/homepage.dart';
+import 'package:ab02012/home_desktop.dart';
+import 'package:ab02012/home_moible.dart';
 import 'package:flutter/material.dart';
 
-
-void main()=> runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: HomePage(),
+        body: Layout(),
       ),
+    );
+  }
+}
+
+class Layout extends StatelessWidget {
+  const Layout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth < 600) {
+          return HomeMobile();
+        } else {
+          return HomeDesktop();
+        }
+      },
     );
   }
 }
