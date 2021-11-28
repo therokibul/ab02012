835159be0abbf36b924f02904be3d13ebbc17cae
therokibul <therokibul@gmail.com>
2021-11-28 15:31:11 +0600

    28-11

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:ab02012/home_desktop.dart';
-import 'package:ab02012/home_moible.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,26 +8,74 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Layout(),
-      ),
+      home: HomePage(),
     );
   }
 }
 
-class Layout extends StatelessWidget {
-  const Layout({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+var changeContainer = true;
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> curvedAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: Duration(seconds: 5),
+      vsync: this,
+    );
+    _animationController.repeat();
+    curvedAnimation = CurvedAnimation(
+        parent: _animationController, curve: Curves.elasticInOut);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        if (constraints.maxWidth < 600) {
-          return HomeMobile();
-        } else {
-          return HomeDesktop();
-        }
-      },
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RotationTransition(
+              turns: Tween<double>(begin: 0, end: 1).animate(curvedAnimation),
+              child: Icon(
+                Icons.star,
+                size: 200,
+              ),
+            ),
+            AnimatedContainer(
+              duration: Duration(
+                seconds: 5,
+              ),
+              height: changeContainer ? 300 : 200,
+              width: changeContainer ? 300 : 60,
+              color: Colors.teal,
+              curve: Curves.bounceInOut,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  changeContainer = !changeContainer;
+                });
+              },
+              child: Text('Do Animation'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
