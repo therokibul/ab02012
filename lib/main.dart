099dd0647b208039d02425394da2e27843284761
therokibul <therokibul@gmail.com>
2021-12-12 15:41:41 +0600

    hero

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+
 import 'package:ab02012/pages/home.dart';
-import 'package:ab02012/pages/second.dart';
-import 'package:ab02012/pages/third.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,54 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CoffeApp(),
+      home: Home(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class CoffeApp extends StatefulWidget {
-  CoffeApp({Key? key}) : super(key: key);
-
-  @override
-  State<CoffeApp> createState() => _CoffeAppState();
-}
-
-class _CoffeAppState extends State<CoffeApp> {
-  List listBody = [
-    Home(),
-    Second(),
-    Third(),
-  ];
-
-  var currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: listBody[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (index){
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard_outlined),
-            label: 'Gift',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.summarize_outlined),
-            label: 'Summar',
-          ),
-        ],
-      ),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,114 +1,30 @@
+import 'dart:js';
+
+import 'package:ab02012/pages/second.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  Home({Key? key}) : super(key: key);
-  List coffeImage = [
-    'https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y29mZmVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1485808191679-5f86510681a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y29mZmVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1498804103079-a6351b050096?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y29mZmVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1507133750040-4a8f57021571?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y29mZmVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1541870730196-cd1efcbf5649?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvZmZlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1541167760496-1628856ab772?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNvZmZlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-  ];
-  List coffeTitle = [
-    'Americano',
-    'Cappuccino',
-    'Latte',
-    'Flat White',
-    'Raf',
-    'Expresso',
-  ];
+  const Home({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              SizedBox(
-                width: 10,
-              ),
-              Column(
-                children: [
-                  Text('Welcome!'),
-                  Text(
-                    'Alex',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              Spacer(),
-              Icon(Icons.storefront_outlined),
-              SizedBox(
-                width: 10,
-              ),
-              Icon(Icons.person_outlined),
-              SizedBox(
-                width: 10,
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.blueGrey.shade700,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(25),
-                topRight: Radius.circular(25),
-              ),
+    return Scaffold(
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> Second()),);
+            },
+            leading: Hero(
+              tag: 'Coffee',
+              child: Image(
+                  image: NetworkImage(
+                      'https://images.unsplash.com/photo-1541167760496-1628856ab772?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNvZmZlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60')),
             ),
-            child: Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(12),
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Select Your Coffee',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: GridView.builder(
-                        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                          maxCrossAxisExtent: 300,
-                          mainAxisSpacing: 10,
-                          crossAxisSpacing: 10,
-                        ),
-                        itemCount: coffeImage.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(25),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image.network(
-                                  coffeImage[index],
-                                  width: 80,
-                                  height: 80,
-                                  fit: BoxFit.cover,
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Text(coffeTitle[index]),
-                              ],
-                            ),
-                          );
-                        }),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
+            title: Text('Coffee'),
+          )
+        ],
+      ),
     );
   }
 }
