f8fffe366848ae18598adbff65c97fc20da37c36
therokibul <therokibul@gmail.com>
2021-12-19 17:10:33 +0600

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:ab02012/pages/home.dart';
+import 'pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,60 +1,57 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
-  Home({Key? key}) : super(key: key);
+  Home({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  List userData = [];
+  List covidData = [];
 
   Future getData() async {
     var response =
-        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+        await http.get(Uri.parse('https://disease.sh/v3/covid-19/countries'));
 
     var responseData = jsonDecode(response.body);
 
     setState(() {
-      userData = responseData;
+      covidData = responseData;
     });
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getData();
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10),
-          itemCount: userData.length,
-          itemBuilder: (context, index) {
-            if (userData == null) {
-              return CircularProgressIndicator();
-            } else {
-              return Card(
-                child: Column(
-                  children: [
-                    Text('Name: ${userData[index]["name"]}'),
-                    Text('UserName: ${userData[index]["username"]}'),
-                    Text('PhoneNumber ${userData[index]["phone"]}'),
-                  ],
+        body: ListView.builder(
+            itemCount: covidData.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Image.network(
+                    
+                    covidData[index]["countryInfo"]["flag"],
+                    height: 80,
+                    width: 90,
+                    fit: BoxFit.cover,
+                  ),
+                  title: Text(covidData[index]["country"]),
+                  subtitle: Text('Total cases: ${covidData[index]["cases"]}'),
                 ),
               );
-            }
-          }),
-    );
+            }));
   }
 }
